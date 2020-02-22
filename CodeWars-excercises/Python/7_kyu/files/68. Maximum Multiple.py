98c03b5d64c9623c5cff2d7346f352c1bcfbe2f3
pumacens <dancortes90@gmail.com>
2020-02-22 08:41:21 -0500

    update excercises

--- CodeWars-excercises/Python/7_kyu/files/68. Maximum Multiple.py
+++ CodeWars-excercises/Python/7_kyu/files/68. Maximum Multiple.py
@@ -1,1 +1,12 @@
-#Not implemented yet
+def max_multiple(divisor, bound):
+    for n in range(bound, divisor, -1):
+        if n % divisor == 0:
+            return n
+
+
+def max_multiple(divisor, bound):
+    return bound - (bound % divisor)
+
+
+def max_multiple(divisor, bound):
+    return bound // divisor * divisor
